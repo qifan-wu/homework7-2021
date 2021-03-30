610b73eb2d69c6f6c174d8dfa84e8d664333d130
qifan-wu <70347591+qifan-wu@users.noreply.github.com>
2021-03-31 02:56:16 +0800

    Update video.js

--- js/video.js
+++ js/video.js
@@ -1,21 +1,20 @@
 var video = document.querySelector('#player1');
 
 window.addEventListener("load", function() {
-	console.log("Good job opening the window")
-
+	console.log("Good job opening the window");
 });
 
 document.querySelector("#play").addEventListener("click",
 function() {
-	console.log("Play Video")
-	video.play()
-	document.querySelector("#volume").innerHTML = video.volume*100 + '%'
+	console.log("Play Video");
+	video.play();
+	document.querySelector("#volume").innerHTML = video.volume*100 + '%';
 });
 
 document.querySelector("#pause").addEventListener("click",
 function() {
-	console.log("Pause Video")
-	video.pause()
+	console.log("Pause Video");
+	video.pause();
 });
 
 document.querySelector("#slower").addEventListener("click", function() {
@@ -29,41 +28,40 @@ document.querySelector("#faster").addEventListener("click", function() {
 });
 
 document.querySelector("#slider").addEventListener("click", function() {
-	console.log("changing volume")
-	console.log(this.value)
-	video.volume = this.value/100
-	document.querySelector("#volume").innerHTML=video.volume*100 + '%'
+	console.log("changing volume");
+	console.log(this.value);
+	video.volume = this.value/100;
+	document.querySelector("#volume").innerHTML=video.volume*100 + '%';
 });
 
 document.querySelector("#mute").addEventListener("click", function() {
 	if (video.muted == false) {
-		console.log("mute")
+		console.log("mute");
 		video.muted = true;
-		this.innerHTML="Unmute"
+		this.innerHTML="Unmute";
 	}
 	else {
-		console.log("unmute")
+		console.log("unmute");
 		video.muted = false;
-		this.innerHTML="Mute"
+		this.innerHTML="Mute";
 	}
 });
 
 document.querySelector("#vintage").addEventListener("click", function() {
-	console.log("Changing CSS")
-	video.classList.add("oldSchool")
+	console.log("Changing CSS");
+	video.classList.add("oldSchool");
 });
-
+''
 document.querySelector("#orig").addEventListener("click", function() {
-	console.log("Removing CSS")
-	video.classList.remove("oldSchool")
+	console.log("Removing CSS");
+	video.classList.remove("oldSchool");
 });
 
 document.querySelector("#skip").addEventListener("click", function() {
-	console.log("skipping")
-	video.play()
-	video.currentTime += 15
-	if (video.currentTime > video.duration) {
-		video.currentTime = 0
+	console.log("skipping ahead");
+	if (video.currentTime < video.duration - 15) {
+		video.currentTime += 15;
 	}
-	console.log(video.currentTime)
+	else {video.currentTime = 0}
+	console.log(video.currentTime);
 });
